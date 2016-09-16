1eb7899f8da000bd8b1e1b890f4343e06cccfcc5
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2016-09-16 13:51:59 +0200

    Add legacy syntax

--- src/main/scala/generic/syntax/package.scala
+++ src/main/scala/generic/syntax/package.scala
@@ -3,6 +3,6 @@ package generic
 package object syntax
   extends AtSyntax
      with AppendSyntax
-     with GetSyntax
-     with EntrySyntax
+     with HMapSyntax
+     with LegacySyntax
      with NatSyntax
